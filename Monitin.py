5eb9963071938750e4dec8416c2bfc6d0b1b4577
cyberdefender123 <40757968+cyberdefender123@users.noreply.github.com>
2018-08-15 15:01:11 +0300

    Update Monitin.py

--- Monitin.py
+++ Monitin.py
@@ -14,7 +14,6 @@ def FileToHash(path):
 
 def CalculateFile():
     path = raw_input('enter file path: ')
-    #print FileToHash(path)
     return FileToHash(path)
 
 
@@ -26,13 +25,12 @@ def CalculateFolder():
     for file in lst_files:
         file_path = mypath + '\\' + file
         if os.path.isfile(file_path):
-            #readfile = open(file_path, 'rb')
             md5 = FileToHash(file_path)
             rate = GetReport(md5)
 
             Results[file] = {'MD5': md5, 'SIZE (KB)': GetSize(file_path), 'CHANGETIME': ChangeTime(file_path), 'EXECUTABLE': IsExecutable(file_path), 'RATE': rate}
 
-    #below is the syntax to return my dictionary in a neat readable table
+    #below is the syntax to return dictionary in a neat readable table
     print "{:<50} {:<50} {:<50} {:<50} {:<50}".format('MD5', 'SIZE (KB)', 'CHANGETIME', 'EXECUTABLE', 'RATE')
     for k, v in Results.iteritems():
         print "{:<50} {:<50} {:<50} {:<50} {:<50}".format(v['MD5'], v['SIZE (KB)'], v['CHANGETIME'], v['EXECUTABLE'], v['RATE'])
@@ -47,23 +45,6 @@ def IsExecutable(file_path):
         executable = False
 
     return executable
-
-#def DictionaryToTable (ResultsDict):
-
-
-'''def CalculateRate(positives, total, isexec):
-
-    pos
-    if data['verbose_msg'] == 'The requested resource is not among the finished, queued or pending scans':
-        rate = 0.9
-    else:
-        if executable
-
-        rate = data['positives'] / data['total']
-        if not executable then divide by 2
-
-    return rate'''
-
 
 
 def GetSize(file_path):
@@ -88,15 +69,12 @@ def GetReport(md5):
     if data['verbose_msg'] == 'The requested resource is not among the finished, queued or pending scans':
         rate = 0.9
     else:
-        # this should be - if executable then rate is as below
         rate = data['positives'] / data['total']
-        # add in here - else: and then if the file is not executable then divide the rate by 2
-
     return rate
 
 
 def main():
-    print '- - Welcome to the super cool Monitin Programme - - '
+    print '- - Welcome to the Monitin Programme - -'
 
     print 'Please select action:'
 
@@ -113,11 +91,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-#my program works!!
-# to make it better, separate theings into separate functions to make it neater. also fix up the date so that it's day, month, year.
-# also fix up the rate thing - to add the executable or not rate - see notes above, and put the if executable or not into its own function.
-#add the sysv argument line at the bottom so that this program can run from cmd with an argument.
-# also add that when searching if a file is executable or not, add that it should check if either the binary begins MZ or the last 4
-# characters are .exe - coz both could be exe so it's better. and maybe display which one it is.
 
