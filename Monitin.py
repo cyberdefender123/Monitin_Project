fd6dc060b592ea71a771dbabb2318f67594bf063
cyberdefender123 <40757968+cyberdefender123@users.noreply.github.com>
2018-08-15 15:31:25 +0300

    Update Monitin.py

--- Monitin.py
+++ Monitin.py
@@ -5,6 +5,7 @@ import requests
 
 
 def FileToHash(path):
+    #receives file path and returns its md5 hash
     f = open(path, 'rb')
     h = hashlib.md5()
     h.update(f.read())
@@ -18,6 +19,7 @@ def CalculateFile():
 
 
 def CalculateFolder():
+    #creates a dictionary of file hash, file size, last changed time, if executable or not and the score
     mypath = raw_input('enter folder path: ')
     lst_files = os.listdir(mypath)
     Results = {}
@@ -37,29 +39,32 @@ def CalculateFolder():
 
 
 def IsExecutable(file_path):
-
+    #determines if file is executable or not
     readfile = open(file_path, 'rb')
     if readfile.read()[:2] == 'MZ':
         executable = True
     else:
         executable = False
-
     return executable
 
 
 def GetSize(file_path):
+    #returns size of file
     complicated_size = os.path.getsize(file_path)
     size = float(complicated_size) / 1024
     return size
 
 
 def ChangeTime(file_path):
+    #calculates datetime from last file change
     changetime = os.path.getctime(file_path)
     readabletime = datetime.fromtimestamp(changetime).strftime('%c')
     return readabletime
 
 def GetReport(md5):
-
+    #sends md5 hash to virustotal and calculates its score for a known file. If file is unknown, returns message. 
+    #the score of the file determines how suspicious it is based on how many of the anti-viruses classified it as suspicious (positives)
+    #divided by the total number of responses from VirusTotal. If file is executable its considered more suspicious, so score is higher.
     url = 'https://www.virustotal.com/vtapi/v2/file/report'
     apikey = '9251624a0029374f3fecf9f1b28b3940a3184704a64dbe66771bb40503778c37'
     params = {'apikey': apikey, 'resource': md5}
@@ -68,6 +73,8 @@ def GetReport(md5):
 
     if data['verbose_msg'] == 'The requested resource is not among the finished, queued or pending scans':
         rate = 0.9
+    if not IsExecutable:
+        rate = rate/2
     else:
         rate = data['positives'] / data['total']
     return rate
